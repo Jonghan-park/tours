1fd62481d68c9b46e1e48a19b04c3466636ecf62
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-19 23:39:40 -0600

    Final update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) =>{
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+
 const fetchTours = async() => {
   setLoading(true);
 
@@ -34,8 +40,18 @@ useEffect(() => {
     </main>
     );
   }
+  if(tours.length === 0){
+    return <main>
+      <div className='title '>
+        <h2>No tours left</h2>
+        <button className='btn' onClick={fetchTours}>
+          refresh
+        </button>
+      </div>
+    </main>
+  }
   return <main>
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour} />
   </main>
   
 }
