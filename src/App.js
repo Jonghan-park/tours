c483f9fddda080de07b68ee9f319fba3f2898ac2
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-19 23:01:17 -0600

    Create Tours.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {useState , useEffect} from 'react';
 import Loading from './Loading';
-import Tours from './Tours'
+import Tours from './Tours';
 import './App.css';
-
-
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
--- src/Loading.js
+++ src/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export const Loading = () => {
+const Loading = () => {
     return (
         <div className='loading'>
             <h1>Loading....</h1>
 
         </div>
-    )
-}
+    );
+};
+
+export default Loading;
