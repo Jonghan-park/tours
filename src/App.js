979ef1ce3bb06dfa482b353f9952f64670b5ead6
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-19 23:14:44 -0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+const fetchTours = async() => {
+  setLoading(true);
+
+  try{
+    const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false);
+    setTours(tours);
+  } catch(error){
+    setLoading(false);
+    console.log(error);
+  }
+  
+};
+
+useEffect(() => {
+  fetchTours();
+}, []);
+
   if (loading) {
     return (
     <main>
@@ -14,6 +34,9 @@ function App() {
     </main>
     );
   }
+  return <main>
+    <Tours tours={tours}/>
+  </main>
   
 }
 
